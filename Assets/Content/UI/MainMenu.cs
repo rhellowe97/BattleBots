e57c0735842acd7b3b9b4d1321e02981524fe189
Ryan Hellowell <hellowellry@gmail.com>
2023-03-24 11:15:08 -0400

    Scene and menu Updates

--- Assets/Content/UI/MainMenu.cs
+++ Assets/Content/UI/MainMenu.cs
@@ -1,6 +1,9 @@
 using CapsuleHands.Singleton;
+using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace CapsuleHands.UI
 {
@@ -8,11 +11,45 @@ namespace CapsuleHands.UI
     {
         [SerializeField] private TMP_InputField nameInputField;
 
+        [SerializeField] private TMP_InputField testSceneField;
+
+        public static string OnlineScene = "";
+
+        private const string TEST_SCENE_KEY = "TestScene";
+
         private void Start()
         {
+            if ( string.IsNullOrEmpty( OnlineScene ) && !string.IsNullOrEmpty( CapsuleNetworkManager.Instance.onlineScene ) )
+            {
+                string[] s = CapsuleNetworkManager.Instance.onlineScene.Split( "/" );
+
+                if ( s.Length > 0 )
+                {
+                    OnlineScene = s[s.Length - 1].Split( "." )[0];
+
+                    testSceneField.text = PlayerPrefs.GetString( TEST_SCENE_KEY, OnlineScene );
+
+                }
+            }
+            else
+            {
+                CapsuleNetworkManager.Instance.onlineScene = OnlineScene;
+
+                testSceneField.text = OnlineScene;
+            }
+
             nameInputField.text = GameManager.Instance.PlayerData.playerName;
 
             nameInputField.onValueChanged.AddListener( GameManager.Instance.UpdatePlayerName );
+        }
+
+        public void LoadTestScene()
+        {
+            PlayerPrefs.SetString( TEST_SCENE_KEY, testSceneField.text.Trim() );
+
+            CapsuleNetworkManager.Instance.onlineScene = testSceneField.text.Trim();
+
+            CapsuleNetworkManager.Instance.StartHost();
         }
     }
 }
